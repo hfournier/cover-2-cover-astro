7dc7ada04d0672a7ad19e2e5cd10e83fecb4b130
Henri Fournier <hfournier@intratela.com>
2026-01-14 16:55:39 -0800

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cover2cover.ca",
+  site: "https://cover2cover-hfdev.netlify.app/",
+
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
-});
+
+  integrations: [
+    sitemap({
+      xslURL: "/rss/styles.xsl"
+    })
+  ]
+})
